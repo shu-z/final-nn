7d53a3dd7597ea3be447dc36d82a7736a46af0e1
Shu Zhang <shu@broadinstitute.org>
2023-03-17 00:23:31 -0700

    chaos

--- test/test_nn.py
+++ test/test_nn.py
@@ -1,4 +1,14 @@
 # TODO: import dependencies and write unit tests below
+import pytest 
+import numpy as np
+from sklearn.metrics import log_loss
+
+
+
+#make an instance of nn class to be used by multiple functions
+
+#make lame test dataset to use 
+
 
 def test_single_forward():
     pass
@@ -10,12 +20,29 @@ def test_single_backprop():
     pass
 
 def test_predict():
+
+    #test that prediction is correct 
     pass
 
 def test_binary_cross_entropy():
-    pass
+        
+    
+    #make random y_true and y_pred
+    y_true=[0, 1, 1, 0, 1, 1, 0]
+    y_pred=[0.1, 0.9, 0.7, 0.2, 0.5, 0.6, 0.3]
+        
+
+    #test with nn model
+    nn_loss=nn_model.loss_function(np.array(y_true), np.array(y_pred))
+    sklearn_loss=log_loss(y_true, y_pred)
+
+	#check that bce loss from our function is close to sklearn loss with reasonable tolerance 
+    assert np.isclose(nn_loss, sklearn_loss, rtol=1e-4), 'BCE loss is not as expected'
+ 
 
 def test_binary_cross_entropy_backprop():
+
+    #do vs hand calculated 
     pass
 
 def test_mean_squared_error():
